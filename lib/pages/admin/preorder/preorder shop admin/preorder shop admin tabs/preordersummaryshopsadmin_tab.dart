206cf3da236de336f6cd85272f64b5266d2d994a
speachofficial <speachofficial@gmail.com>
2023-08-01 23:53:54 +0800

    summary tabs admin

--- lib/pages/admin/preorder/preorder shop admin/preorder shop admin tabs/preordersummaryshopsadmin_tab.dart
+++ lib/pages/admin/preorder/preorder shop admin/preorder shop admin tabs/preordersummaryshopsadmin_tab.dart
@@ -3,124 +3,68 @@ import 'package:ecoop3/widgets/empty.dart';
 import 'package:ecoop3/widgets/loadinghairil.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
 import '../../../../../utils/colors.dart';
 import '../../../../../utils/my_package.dart';
 import '../../../../../widgets/smalltext.dart';
+import 'today.dart';
+import 'tommorow.dart';
 
-class MyShopSummaryContent extends StatelessWidget {
+const myShopsTabs = [
+  Tab(
+    text: 'Today',
+  ),
+  Tab(
+    text: 'Tommorrow',
+  ),
+];
+
+class MyShopSummaryContent extends StatefulWidget {
   const MyShopSummaryContent({Key? key}) : super(key: key);
 
   @override
+  State<MyShopSummaryContent> createState() => _MyShopSummaryContentState();
+}
+
+class _MyShopSummaryContentState extends State<MyShopSummaryContent>
+    with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    TabController tabController =
+        TabController(length: myShopsTabs.length, vsync: this);
+    return Column(
       children: [
-        Padding(
-          padding: EdgeInsets.only(bottom: 32.h),
-          child: SmallText(
-            text:
-                'Order For ${DateFormat('EEEE, dd / MM').format(DateTime(now.year, now.month, now.day).add(const Duration(days: 2)).toUtc())}',
-            size: 48.sp,
-            color: AppColors.c000000_100,
-            fontWeight: FontWeight.bold,
+        TabBar(
+            controller: tabController,
+            labelColor: AppColors.c003FFF_100,
+            unselectedLabelColor: AppColors.c333333_100,
+            labelStyle: GoogleFonts.inter(
+              fontWeight: FontWeight.bold,
+              fontSize: 48.sp,
+            ),
+            indicatorSize: TabBarIndicatorSize.label,
+            indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(
+              color: AppColors.c003FFF_100,
+              width: 6.h,
+            )),
+            tabs: myShopsTabs),
+        SizedBox(
+          height: 5.h,
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: tabController,
+            children: const [
+              TodayOrders(),
+              TommorrowOrders()
+            ],
           ),
         ),
-        FutureBuilder<QuerySnapshot>(
-          future: FirebaseFirestore.instance
-              .collection('preorder_products')
-              .get(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot> productsnapshot) {
-            if (productsnapshot.connectionState == ConnectionState.waiting) {
-              return LoadingHairil();
-            }
-            if (!productsnapshot.hasData || productsnapshot.data == null) {
-              return const EmptyHairil();
-            }
-
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: productsnapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                return FutureBuilder<double>(
-                  future: calculateTotalItemQuantity(
-                      productsnapshot.data!, index, context),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<double> totalQuantitySnapshot) {
-                    if (productsnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return LoadingHairil();
-                    }
-                    if (totalQuantitySnapshot.hasError) {
-                      return Text('Error: ${totalQuantitySnapshot.error}');
-                    }
-
-                    final totalitemquantity = totalQuantitySnapshot.data ?? 0;
-
-                    return Container(
-                      margin: EdgeInsets.only(bottom: 30.h),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 350.h,
-                            width: 350.w,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20.r),
-                              color: Colors.amber,
-                              image: DecorationImage(
-                                image: NetworkImage(productsnapshot
-                                    .data!.docs[index]
-                                    .get('url')),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 45.w,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SmallText(
-                                text: productsnapshot.data!.docs[index]
-                                    .get('name'),
-                                size: 64.sp,
-                                color: AppColors.c333333_100,
-                                fontWeight: FontWeight.w600,
-                              ),
-                              Row(
-                                children: [
-                                  SmallText(
-                                    text: 'Total : ',
-                                    size: 48.sp,
-                                    fontWeight: FontWeight.w600,
-                                    color: AppColors.c333333_100,
-                                  ),
-                                  SmallText(
-                                    text: totalitemquantity.toStringAsFixed(0),
-                                    size: 48.sp,
-                                    fontWeight: FontWeight.w600,
-                                    color: AppColors.cC8151D_100,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-            );
-          },
-        ),
       ],
-    ));
+    );
   }
 
   Future<double> calculateTotalItemQuantity(
